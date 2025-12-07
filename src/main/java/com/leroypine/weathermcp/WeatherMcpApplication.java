a1ac2733152fb19b8431b0fa389eac37b5bfe4ed
Squirrel <35068966+LeroyPine@users.noreply.github.com>
2025-12-07 14:12:36 +0800

    添加 Spring Boot 项目源代码

---
+++ src/main/java/com/leroypine/weathermcp/WeatherMcpApplication.java
@@ -0,0 +1,11 @@
+package com.leroypine.weathermcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMcpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherMcpApplication.class, args);
+    }
+}
